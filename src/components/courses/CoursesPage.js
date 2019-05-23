10462a6d0c1b77add93665b0ccd3b489a2ffe4cb
kristaps <kristapsbulssusons@gmail.com>
2019-05-24 00:34:16 +0300

    1

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,33 +1,32 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
-  constructor(props) {
-  super(props);
-    this.state = {
-      img: '',
+  state = {
+    course: {
+      title: "",
     }
-    this.onchange=this.onChange.bind(this);
   }
-  onChange(e) {
-    let files=e.target.files;
-    console.warn("data file", files);
-    let reader=new FileReader();
-    reader.readAsDataURL(files[0]);
-    reader.onload=(e)=>{
-      const url="test"
-      const formData={file: e.target.result}
-      console.warn("img data", e.target.result);
-      return fetch(url, formData).then(response => console.warn("result", response))
 
-    }
+  handleChanges = e => {
+    const course = {...this.state.course, title: e.target.value};
+    this.setState({course});
+  };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    alert(this.state.course.title)
   }
+
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
-        <input className="UploadButton" type="file" name="file" onChange={(e) => this.onChange(e)}/>
-      </div>
+        <h3>Add course</h3>
+        <input type="text"
+        value={this.state.course.title}
+        onChange={this.handleChanges}/>
+        <input type="submit" value="Submit"/>
+      </form>
     )
 
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from './redux/configureStore';
+import {Provider as ReduxProvider } from 'react-redux';
 
-render(<Router>
-  <App />
-  </Router>, document.getElementById("app"));
+const store = configureStore();
+
+
+render(
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
+  document.getElementById("app"));
